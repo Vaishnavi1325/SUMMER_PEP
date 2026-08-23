61ff0252e9532b8645d1d5bb5e12a5531e6424d1
Vaishnavi <vaishnavishukla9578b@gmail.com>
2026-08-24 00:50:43 +0530

    Time: 0 ms (100%), Space: 10.3 MB (23.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,28 +1,27 @@
 class Solution {
 private:
-    bool isValid(char ch){
-        if((ch>='a'&& ch <='z') || (ch>='A'&& ch<='Z') || (ch>='0'&& ch<='9'))
+bool isValid(char &ch){
+     if((ch>='a'&& ch <='z') || (ch>='A'&& ch<='Z') || (ch>='0'&& ch<='9'))
         return 1;
         return 0;
-    }
+}
 public:
     bool isPalindrome(string s) {
-        int n =s.size();
-        string temp="";
-        for(int i=0;i<n;i++){
-            if(isValid(s[i])) temp.push_back(s[i]);
+        string sr="";
+        for(int i=0;i<s.size();i++){
+            if(isValid(s[i])) {
+                s[i]=(char)tolower(s[i]);
+                sr.push_back(s[i]);
+            }
         }
-        for(int i=0;i<temp.size();i++){
-            temp[i]=tolower(temp[i]);
-        }
-
-        int i=0;
-        int j=temp.size()-1;
-        while(i<j){
-            if(temp[i]!=temp[j]) return false;
-            i++;
-            j--;
+        int l=0;
+        int r=sr.size()-1;
+        while(l<r){
+            if(sr[l]!=sr[r]) return false;
+            l++;
+            r--;
         }
         return true;
+
     }
 };
